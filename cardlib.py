7b87194650c5090c1ce7d2f04f5ee88574ff8c04
john-tran <johntran1@hotmail.se>
2023-02-07 15:17:13 +0100

    Done

--- cardlib.py
+++ cardlib.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 from enum import Enum
 from abc import ABCMeta, abstractmethod
-
+import random
 
 class Suit(Enum):
     Hearts = 4
@@ -9,10 +9,8 @@ class Suit(Enum):
     Clubs = 2
     Diamonds = 1
 
-
-print(Suit.Hearts)
-
-print(Suit.Hearts.value)
+    def __str__(self):
+        return self.name
 
 
 class PlayingCard(metaclass=ABCMeta):
@@ -28,6 +26,7 @@ class PlayingCard(metaclass=ABCMeta):
             return self.suit.value < other.suit.value
         else:
             return self.get_value() < other.get_value()
+
     def __eq__(self, other):
         if self.get_value() == other.get_value() and self.suit.value == other.suit.value:
             return True
@@ -40,6 +39,9 @@ class NumberedCard(PlayingCard):
         super().__init__(suit)
         self.value = value
 
+    def __str__(self):
+        return f'{self.value} of {self.suit}'
+
     def get_value(self):
         return self.value
 
@@ -47,6 +49,9 @@ class NumberedCard(PlayingCard):
 class JackCard(PlayingCard):
     def __init__(self, suit):
         super().__init__(suit)
+
+    def __str__(self):
+        return f' Jack of {self.suit}'
 
     def get_value(self):
         return 11
@@ -56,6 +61,9 @@ class QueenCard(PlayingCard):
     def __init__(self, suit):
         super().__init__(suit)
 
+    def __str__(self):
+        return f' Queen of {self.suit}'
+
     def get_value(self):
         return 12
 
@@ -63,6 +71,9 @@ class QueenCard(PlayingCard):
 class KingCard(PlayingCard):
     def __init__(self, suit):
         super().__init__(suit)
+
+    def __str__(self):
+        return f' King of {self.suit}'
 
     def get_value(self):
         return 13
@@ -72,8 +83,45 @@ class AceCard(PlayingCard):
     def __init__(self, suit):
         super().__init__(suit)
 
+    def __str__(self):
+        return f' Ace of {self.suit}'
+
     def get_value(self):
         return 1, 14
 
 
-print(JackCard(Suit.Hearts) < JackCard(Suit.Spades))
+print(type(KingCard(Suit.Hearts)))
+
+
+# StandardDeck
+class StandardDeck:
+    def __init__(self):
+        self.deck = []
+        for i in Suit:
+            self.deck.append(AceCard(i))
+            for j in range(2, 11):
+                self.deck.append(NumberedCard(i, j))
+            self.deck.append(JackCard(i))
+            self.deck.append(QueenCard(i))
+            self.deck.append(KingCard(i))
+
+    def draw(self):
+        return self.deck.pop(0)
+
+    def shuffle(self):
+        return random.shuffle(self.deck)
+
+
+h = StandardDeck()
+
+
+class Hand:
+    
+
+
+print(h)
+print(h.draw())
+
+# def shuffle(self):
+
+# def draw(self):
