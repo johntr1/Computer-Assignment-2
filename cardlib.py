d4fa47cc35a8ee1bc309264567fdba9d6f38c285
john-tran <johntran1@hotmail.se>
2023-02-07 12:42:05 +0100

    fixat mer

--- cardlib.py
+++ cardlib.py
@@ -16,6 +16,7 @@ class PlayingCard(metaclass=ABCMeta):
 
     @abstractmethod
     def get_value(self):
+        return self.value
 
 
 
@@ -27,20 +28,41 @@ class NumberedCard(PlayingCard):
     def get_value(self):
         return self.value
 
+    def __lt__(self, other):
+        if self.get_value() == other.get_value():
+            return self.suit.value < other.suit.value
+        else:
+            return self.get_value() < other.get_value()
+
 class JackCard(PlayingCard):
     def __init__(self, suit):
         self.suit = suit
 
     def get_value(self):
-        
+        return 11
+
 
 class QueenCard(PlayingCard):
     def __init__(self, suit):
         self.suit = suit
 
+    def get_value(self):
+        return 12
+
 class KingCard(PlayingCard):
     def __init__(self, suit):
         self.suit = suit
+
+    def get_value(self):
+        return 13
+
+    def __lt__(self, other):
+        if self.get_value() == other.get_value():
+            return self.suit.value < other.suit.value
+        else:
+            return self.get_value() < other.get_value()
+
+
 
 class AceCard(PlayingCard):
     def __init__(self, suit):
@@ -48,3 +70,4 @@ class AceCard(PlayingCard):
 
 
 
+print(NumberedCard(4, Suit.Hearts))
