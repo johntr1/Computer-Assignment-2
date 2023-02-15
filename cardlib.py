5410e917399cfb086c60adb0ced5710586babed7
Martind1d <martin.diderholm@gmail.com>
2023-02-15 16:51:22 +0100

    new

--- cardlib.py
+++ cardlib.py
@@ -142,14 +142,55 @@ class PokerHand:
     def __init__(self, cards):
         self.cards = cards
 
-    # for i in range(5):
-    #    if h.cards[i].get_value() == h.cards[i+1].get_value()-1:
-    #         if cards[i].suit.value == cards[i+1].suit.value:
-    #             if i ==4:
-    #                 return value=9
-    #             continue
-    #         else break
-    #    else break
+
+    def check_straight_flush(self):
+        """
+        Checks for the best stright flush in a list of cards
+
+        :return:
+        """
+        #works if ace is sett as 14, kanske skulle använda oss av två lista av korten i handen
+        vals=[]
+        for c in cards:
+            if c.get_value()==14:
+                cards.append((1,c.suit))
+
+        for card in reversed(cards):
+            for k in range(1, 5):
+                if (card.get_value()-k , card.suit) not in cards:
+                    straight_flush=False
+                    break
+            straight_flush=True
+            if straight_flush:
+                return [card.get_value(), card.suit]
+
+    def check_full_house_three_2pair_pair(self):
+        count=[0]*len(cards)
+        for i, card1 in enumerate(cards):
+            for card2 in cards:
+                if card1.get_value()==card2.get_value():
+                    count[i]=+1
+        if 2 in count and 3 in count:
+            #Finds the position of the three of a kind and get what value it has
+            threes_indices=[i for i, x in enumerate(count) if x==3]
+            threes=cards[threes_indices]
+            threes=reversed(threes.sort())
+            return [Fullhouse, threes[0].get_value]
+        elif 3 in count:
+            # Finds the position of the three of a kind and get what value it has
+            threes_indices = [i for i, x in enumerate(count) if x == 3]
+            threes = cards[threes_indices]
+            threes = reversed(threes.sort())
+            return [three, threes[0].get_value]
+        elif len([i for i in count if i==2]) >= 2:
+            # Finds the position of the pars of a kind and get what value and suit it has
+            pair_indices=[i for i, x in enumerate(count) if x==2]
+            pairs=cards[par_indices]
+            pairs=reversed(pair.sort())
+
+        elif 2 in count:
+
+
 
 
 # if h.cards[i].get_value() == h.cards[-2]+1 and h.cards[-2].get_value() == h.cards[-3].get_value()+1 and h.cards[-4].get_value() == h.cards[-5].get_value()+1:
